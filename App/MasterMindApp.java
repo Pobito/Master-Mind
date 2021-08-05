bf0f9737abe9f5b68fcf9efb480c736c845d5616
Amc210 <annamarin513@gmail.com>
2021-08-05 10:54:33 +0200

    Menú
    Menú con ítems

--- App/MasterMindApp.java
+++ App/MasterMindApp.java
@@ -6,7 +6,6 @@ public class MasterMindApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		Juego juego = new Juego();
 	}
 
--- dto/Juego.java
+++ dto/Juego.java
@@ -60,6 +60,117 @@ public class Juego extends JFrame {
 				juego.setLayout(null);
 				setContentPane(juego);
 				
+				// Barra menu
+				JMenuBar menu = new JMenuBar();
+				setJMenuBar(menu);
+
+				// Apartado archivo
+				JMenu menuArchivo = new JMenu("Archivo");
+				menu.add(menuArchivo);
+
+				// items
+				JMenuItem nuevoJuego = new JMenuItem("Nuevo juego");
+				menuArchivo.add(nuevoJuego);
+
+				JMenuItem salir = new JMenuItem("Salir");
+				menuArchivo.add(salir);
+				salir.addActionListener(new ActionListener() { // Cuando clique mostrara el sistema seleccionado
+					public void actionPerformed(ActionEvent e) {
+						System.exit(0);
+					}
+				});
+
+				// menu ayuda
+				JMenu ayuda = new JMenu("Ayuda");
+				menu.add(ayuda);
+
+				JMenu comoJugar = new JMenu("Como jugar");
+				ayuda.add(comoJugar);
+
+				JMenuItem elementos = new JMenuItem("Elementos del juego");
+				comoJugar.add(elementos);
+				elementos.addActionListener(new ActionListener() { // Cuando clique mostrara el sistema seleccionado
+					public void actionPerformed(ActionEvent e) {
+						JOptionPane.showMessageDialog(null,
+								"Tablero compuesto por:\r\n"
+										+ "· 12 filas de agujeros grandes para las clavijas de colores.\r\n"
+										+ "· 12 filas de agujeros pequeños para las espigas blancas y negras.\r\n"
+										+ "· 5 agujeros para ocultar con el escudo el código secreto.\r\n"
+										+ "· 2 filas para las clavijas rojas y azules, para anotar los tanteos.\r\n"
+										+ "· 1 Compartimento grande para las clavijas de colores.\r\n"
+										+ "· 1 Compartimento para las espigas blancas y negras.\r\n" + "Piezas:\r\n"
+										+ "· 1 Escudo para ocultar el código secreto.\r\n"
+										+ "· 192 clavijas grandes de diferentes colores (Rojas, azules, verdes,\r\n"
+										+ "amarillas, rosas, blancas, negras y marrones).\r\n"
+										+ "· 60 espigas blancas y negras para la clave.\r\n"
+										+ "· 4 espigas de tanteo, 2 rojas y 2 azules.");
+					}
+				});
+
+				JMenuItem comoSeJuega = new JMenuItem("Como se juega");
+				comoJugar.add(comoSeJuega);
+				comoSeJuega.addActionListener(new ActionListener() { // Cuando clique mostrara el sistema seleccionado
+					public void actionPerformed(ActionEvent e) {
+						JOptionPane.showMessageDialog(null,
+								"En Mastermind compiten 2 jugadores, uno de ellos creará un código\r\n"
+										+ "oculto con 5 clavijas de colores, pudiendo hacer las combinaciones con los 8\r\n"
+										+ "colores disponibles e incluso repitiendo color si lo desea.\r\n"
+										+ "El código de colores debe de ocultarse con el escudo para que no\r\n"
+										+ "pueda verlo el oponente, que deberá acertar en el menor número posible de\r\n"
+										+ "jugadas la clave para obtener una buena puntuación. Para descifrar el código\r\n"
+										+ "secreto de colores el jugador deberá ir probando combinaciones aleatorias de\r\n"
+										+ "colores, y en cada combinación, el jugador contrario debe darle pistas mediante\r\n"
+										+ "las espigas blancas y negras.\r\n"
+										+ "Por cada clavija acertada en color y posición, colocará una espiga negra,\r\n"
+										+ "y por cada color acertado pero en un lugar equivocado colocara una espiga\r\n"
+										+ "blanca.");
+					}
+				});
+
+				JMenuItem puntuacion = new JMenuItem("Puntuación");
+				comoJugar.add(puntuacion);
+				puntuacion.addActionListener(new ActionListener() { // Cuando clique mostrara el sistema seleccionado
+					public void actionPerformed(ActionEvent e) {
+						JOptionPane.showMessageDialog(null, "La forma de puntuación es bastante sencilla: se puede convenir antes de\r\n"
+								+ "empezar de cuantos juegos va a ser la partida, cada jugador tiene un color\r\n"
+								+ "distinto de espiga de tanteo, rojo o azul, que colocará en el número 1 de la fila\r\n"
+								+ "vertical de tanteo.\r\n"
+								+ "Al final de cada juego, el jugador que ha creado el código ganará tantos\r\n"
+								+ "puntos como filas haya cubierto el oponente hasta que ha conseguido descifrar\r\n"
+								+ "el código. Es decir, si ha necesitado probar 7 combinaciones distintas antes de\r\n"
+								+ "acertar, estos son los puntos que gana el jugador contrario, que se anotarán\r\n"
+								+ "cambiando la posición de la clavija de tanteo correspondiente.\r\n"
+								+ "En el caso de que el jugador que ha creado el código se equivoque al\r\n"
+								+ "dar las pistas con las espigas blancas y negras, deberá repetir la serie y\r\n"
+								+ "además tendrá que dar 3 puntos al jugador contrario. Los jugadores se\r\n"
+								+ "alternarán para crear el código secreto e irán anotando sus puntuaciones con\r\n"
+								+ "las espigas de tanteo hasta terminar la partida.");
+					}
+				});
+
+				// menu acerca de
+				JMenu acercaDe = new JMenu("Acerca de");
+				ayuda.add(acercaDe);
+
+				JMenuItem consisteJuego = new JMenuItem("En que consiste el juego?");
+				acercaDe.add(consisteJuego);
+				consisteJuego.addActionListener(new ActionListener() { // Cuando clique mostrara el sistema seleccionado
+					public void actionPerformed(ActionEvent e) {
+						JOptionPane.showMessageDialog(null, "Mastermind es un juego de habilidad y lógica que consiste en descubrir\r\n"
+								+ "una secuencia de colores oculta. Es un clásico que lleva muchos años en el\r\n"
+								+ "mercado.");
+					}
+				});
+
+
+				JMenuItem programadores = new JMenuItem("Programadores");
+				acercaDe.add(programadores);
+				programadores.addActionListener(new ActionListener() { // Cuando clique mostrara el sistema seleccionado
+					public void actionPerformed(ActionEvent e) {
+						JOptionPane.showMessageDialog(null, "Arián Pobo \nAnna Marín \nSergio Aragón");
+					}
+				});
+				
 				boton = new JButton("Seleccionar");
 				boton.setBounds(10,52,215,47);
 				juego.add(boton);
